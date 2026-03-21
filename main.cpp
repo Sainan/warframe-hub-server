6cd96c8fc3bff4110620690a7097415267f8b3e2
Sainan <63328889+Sainan@users.noreply.github.com>
2026-03-21 19:02:57 +0100

    Support < U18.18 clients

--- main.cpp
+++ main.cpp
@@ -52,6 +52,7 @@ static bool is_u32_or_below(const std::string_view& salt)
 		|| salt == "b471e49539930dc9b5a131e6247c7387D"
 		|| salt == "b471e49539930dc9b5a131e6247c7387B"
 		|| salt == "b471e49539930dc9b5a131e6247c7387A"
+		|| salt == "6f7fd17e0eb641abQ"
 		;
 }
 
@@ -490,11 +491,15 @@ int main(int argc, const char** argv)
 							salt = "b471e49539930dc9b5a131e6247c7387B"; // < U27 && >= U23
 							if (crc32::hash((const uint8_t*)salt.data(), salt.size(), initial) != chksum)
 							{
-								salt = "b471e49539930dc9b5a131e6247c7387A"; // < U23
+								salt = "b471e49539930dc9b5a131e6247c7387A"; // < U23 && >= U18.18
 								if (crc32::hash((const uint8_t*)salt.data(), salt.size(), initial) != chksum)
 								{
-									std::cout << addr.toString() << " - Checksum mismatch: " << string::bin2hex(data) << std::endl;
-									return;
+									salt = "6f7fd17e0eb641abQ"; // < U18.18
+									if (crc32::hash((const uint8_t*)salt.data(), salt.size(), initial) != chksum)
+									{
+										std::cout << addr.toString() << " - Checksum mismatch: " << string::bin2hex(data) << std::endl;
+										return;
+									}
 								}
 							}
 						}
